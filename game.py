f83fe2d4e110ed621b16752d2cebc1e93d02e465
juniorta <ttjrbiz@gmail.com>
2026-07-17 19:05:28 -0400

    added a game ball

--- game.py
+++ game.py
@@ -4,20 +4,25 @@ import pygame
 pygame.init()
 
 #Game graphics
-screen= pygame.display.set_mode((640, 640))
+screen= pygame.display.set_mode((1000, 640))
 background_color = (17, 17, 17)
 
 
 left_player= pygame.image.load('girl.png').convert()
 right_player= pygame.image.load('dude.png').convert()
+ball= pygame.image.load("ball.png").convert()
+ball= pygame.transform.scale(ball, (20, 20))
 
 running= True
+ball_position= 504 #place in the middle when game starts
+
 
 while running:
 
     screen.fill(background_color)
-    screen.blit(left_player, (50, 10))
-    screen.blit(right_player, (590, 400))
+    screen.blit(left_player, (150, 266))
+    screen.blit(right_player, (800, 274))
+    screen.blit(ball, (ball_position, 306))
 
 
     for event in pygame.event.get():
